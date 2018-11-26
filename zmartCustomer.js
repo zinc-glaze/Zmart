f7c49730b409fc1897537fd81aa13125b128851f
Zinc Glaze <zekegraves@gmail.com>
2018-11-26 18:35:18 -0500

    created basic customer interface

--- zmartCustomer.js
+++ zmartCustomer.js
@@ -15,26 +15,68 @@ var Table = require('cli-table');
 //Main process
 connection.connect(function(err) {
   if (err) throw err;
-  listInventory();
+  customer();
 });
 
 
 
 //Functions
-function listInventory() {
+function customer() {
   connection.query("SELECT * FROM products", function(err, res) {
     if (err) throw err;
     // instantiate table
     var table = new Table({
-        head: ['ID', 'Name', 'Price']
-      , colWidths: [5, 22, 8]
+        head: ['ID', 'Name', 'Price'], 
+        colWidths: [5, 22, 8]
     });
+    //populate table
     for (i=0; i < res.length; i++) {
       table.push([res[i].item_id, res[i].product_name, res[i].price]);
     }
-    console.log(table.toString()); 
-    connection.end();
+    //print inventory
+    console.log(table.toString() + "\n");
+    //user input
+    inquirer
+    .prompt([
+      {
+      type: "input",
+      message: "Enter the ID of the product you would like to buy.",
+      name: "id"
+      },
+      {
+      type: "input",
+      message: "Enter the quantity of this item you would like to purchase.",
+      name: "quantity"
+      }
+    ])
+    .then(function(userInput) {
+      if (userInput.quantity > res[userInput.id -1].stock_quantity) {
+      console.log("Insufficient stock!");
+      return;
+      }
+      else {
+        var newQuantity = parseFloat(res[userInput.id -1].stock_quantity - userInput.quantity);
+        connection.query(
+          "UPDATE products SET ? WHERE ?",
+          [
+            {
+              stock_quantity: newQuantity
+            },
+            {
+              item_id: userInput.id
+            }
+          ],
+          function(error) {
+            if (error) throw error;
+            console.log("Total order amount: " + (userInput.quantity * res[userInput.id - 1].price));
+            connection.end();
+          }
+        );
+      }
+    });
   });
 }
+
+
 
 
