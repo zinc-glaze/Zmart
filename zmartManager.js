b1b52bb05b27c9de7f2c55fa14c8385aa616e832
Zinc Glaze <zekegraves@gmail.com>
2018-11-27 13:13:15 -0500

    finished Manager app

--- zmartManager.js
+++ zmartManager.js
@@ -61,16 +61,128 @@ function viewProducts() {
     if (err) throw err;
     // instantiate table
     var table = new Table({
-        head: ['ID', 'Name', 'Price', 'Quantity'], 
-        colWidths: [6, 25, 10, 10]
+        head: ['ID', 'Name', 'Department','Price', 'Quantity'], 
+        colWidths: [6, 25, 20, 10, 10]
     });
     //populate table
     for (i=0; i < res.length; i++) {
-      table.push([res[i].item_id, res[i].product_name, "$" + res[i].price, res[i].stock_quantity]);
+      table.push([res[i].item_id, res[i].product_name, res[i].department_name, "$" + res[i].price, res[i].stock_quantity]);
     }
     //print inventory
     console.log("\n" + table.toString() + "\n");
     //call function
     menuOptions();
+  });
+}
+
+function viewLow() {
+  connection.query("SELECT * FROM products WHERE stock_quantity < '5'", function(err, res) {
+    if (err) throw err;
+    // instantiate table
+    var table = new Table({
+      head: ['ID', 'Name', 'Department','Price', 'Quantity'], 
+      colWidths: [6, 25, 20, 10, 10]
+    });
+    //populate table
+    for (i=0; i < res.length; i++) {
+      table.push([res[i].item_id, res[i].product_name, res[i].department_name, "$" + res[i].price, res[i].stock_quantity]);
+    }
+    //print inventory
+    console.log("\n" + table.toString() + "\n");
+    //call function
+    menuOptions();
+  });
+}
+
+function addInventory() {
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    //user input
+    inquirer
+    .prompt([
+      {
+      type: "input",
+      message: "Enter the ID of the product you would like to re-stock: ",
+      name: "id"
+      },
+      {
+      type: "input",
+      message: "Enter the quantity you would like to add to the stock: ",
+      name: "quantity"
+      }
+    ])
+    .then(function(userInput) {
+      var newQuantity = parseInt(res[userInput.id -1].stock_quantity) + parseInt(userInput.quantity);
+      connection.query(
+        "UPDATE products SET ? WHERE ?",
+        [
+          {
+            stock_quantity: newQuantity
+          },
+          {
+            item_id: userInput.id
+          }
+        ],
+        function(error) {
+          if (error) throw error;
+          console.log("\nNew Quantity: " + newQuantity + "\n");
+          menuOptions();
+        }
+      );
+    });
+  });
+}
+
+function addProduct() {
+  inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "Enter the product name:",
+      name: "product_name"
+    },
+    {
+      type: "input",
+      message: "Enter the department name:",
+      name: "department_name"
+    },
+    {
+      type: "input",
+      message: "Enter the product price:",
+      name: "price",
+      validate: function(value) {
+        if (isNaN(value) === false) {
+          return true;
+        }
+        return false;
+      }
+    },
+    {
+      type: "input",
+      message: "Enter the stock quantity:",
+      name: "stock_quantity",
+      validate: function(value) {
+        if (isNaN(value) === false) {
+          return true;
+        }
+        return false;
+      }
+    }
+  ])
+  .then(function(res) {
+    var query = connection.query(
+      "INSERT INTO products SET ?",
+      {
+        product_name: res.product_name,
+        department_name: res.department_name,
+        price: res.price,
+        stock_quantity: res.stock_quantity,
+      },
+      function(err, res) {
+        if (err) throw err;
+        console.log("Your product was added successfully!\n");
+        menuOptions();
+      }
+    );
   });
 }
